57e8c069bc26cdaabf081ab03f2482fa7ebe0ba1
warnerwarner <tom.p.a.warner@gmail.com>
2020-03-07 14:56:12 +0000

    change data loc

--- binary_experiments/heatmap_maker.py
+++ binary_experiments/heatmap_maker.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-base_dir = '/camp/home/warnert/working/Recordings/binary_pulses/200303/2020-03-03_16-44-23'
-trialbank_loc = '/camp/home/warnert/working/Recordings/binary_pulses/200303/2020-03-03trial_names_dorsal.txt'
+base_dir = '/camp/home/warnert/working/Recordings/binary_pulses/200303/2020-03-03_19-57-03'
+trialbank_loc = '/camp/home/warnert/working/Recordings/binary_pulses/200303/2020-03-03trial_names_ventral.txt'
 rec = br.Binary_recording(base_dir, 32, trialbank_loc)
 
 rec.set()
