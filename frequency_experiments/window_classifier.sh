2a87649f000ae6da992b75935628320752ffbcd7
warnerwarner <tom.p.a.warner@gmail.com>
2020-03-31 17:53:37 +0100

    pass in array

--- frequency_experiments/window_classifier.sh
+++ frequency_experiments/window_classifier.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=heatmaps
+#SBATCH --job-name=classifiyin
 #SBATCH --array=0-599		## N_TASKS_TOTAL: SET THIS TO THE NUMBER OF INDEPENDENT JOBS,TYPICLLAY 100,SEE BELOW N_TASKS_TOTAL
 #SBATCH --output=/home/camp/warnert/bash_scripts/frequency_experiments/window_class_%a.out
 #SBATCH --error=/home/camp/warnert/bash_scripts/frequency_experiments/window_class_%a.err
@@ -9,4 +9,4 @@
 #SBATCH --mem=50G
 #SBATCH --partition=cpu
 
-python window_classifier.py
+python window_classifier.py $SLURM_ARRAY_TASK_ID
