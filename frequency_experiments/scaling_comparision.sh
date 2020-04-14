8f253b86a7ad23821f049db59f4bdb5ecc63274c
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-14 18:07:54 +0100

    Change error and output name

--- frequency_experiments/scaling_comparision.sh
+++ frequency_experiments/scaling_comparision.sh
@@ -2,8 +2,8 @@
 #
 #SBATCH --job-name=classifiyin
 #SBATCH --array=0-3		## N_TASKS_TOTAL: SET THIS TO THE NUMBER OF INDEPENDENT JOBS,TYPICLLAY 100,SEE BELOW N_TASKS_TOTAL
-#SBATCH --output=/home/camp/warnert/bash_scripts/frequency_experiments/window_class_diff_%a.out
-#SBATCH --error=/home/camp/warnert/bash_scripts/frequency_experiments/window_class_diff_%a.err
+#SBATCH --output=/home/camp/warnert/bash_scripts/frequency_experiments/scaling_class_diff_%a.out
+#SBATCH --error=/home/camp/warnert/bash_scripts/frequency_experiments/scaling_class_diff_%a.err
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=50G
