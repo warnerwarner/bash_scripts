07522487fa51f2e586517f84dd1d51086a8794ed
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-17 12:59:12 +0100

    Change to difference response

--- frequency_experiments/all_v_all_classifier.py
+++ frequency_experiments/all_v_all_classifier.py
@@ -31,8 +31,9 @@ repeats = 1000
 trial_names = ['2Hz_A', '5Hz_A', '10Hz_A', '15Hz_A', '20Hz_A',
                '2Hz_B', '5Hz_B', '10Hz_B', '15Hz_B', '20Hz_B',
                '2Hz_C', '5Hz_C', '10Hz_C', '15Hz_C', '20Hz_C',
-               '2Hz_D', '5Hz_D', '10Hz_D', '15Hz_D', '20Hz_D',
-               '2Hz_blank', '5Hz_Blank', '10Hz_Blank', '15Hz_Blank', '20Hz_Blank']
+               '2Hz_D', '5Hz_D', '10Hz_D', '15Hz_D', '20Hz_D']
+
+blanks = ['2Hz_blank', '5Hz_Blank', '10Hz_Blank', '15Hz_Blank', '20Hz_Blank']
 trial1_index = int(list(sys.argv)[1])
 trial1 = trial_names[trial1_index]
 
@@ -42,7 +43,14 @@ classifier.recordings = [rec1, rec2, rec3, rec4, rec5, rec6]
 accs = []
 for trial2 in trial_names:
     if trial2 != trial1:
-        classifier.make_unit_response([trial1, trial2], baseline=False)
+        trial1_freq = trial1.split('_')[0]
+        trial2_freq = trial2.split('_')[0]
+        for i in blanks:
+            if trial1_freq in i:
+                trial1_blank = i
+            if trial2_freq in i:
+                trial2_blank = i
+        classifier.make_difference_response([trial1, trial2], [trial1_blank, trial2_blank],  baseline=False)
         trial_accs = []
         for i in tqdm(range(repeats)):
             classifier.window_classifier([trial1, trial2], 0.0, 0.5, baseline=False)
