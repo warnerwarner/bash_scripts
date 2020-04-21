85858c1027e4a5de2782416bc64f637dcdb4a357
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-21 20:19:23 +0100

    jULIE setting script test

---
+++ jULIE_recordings/jULIE_recording_setting.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('/home/camp/warnert/neurolytics')
+import threshold_recording as tr
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+tc = tr.Threshold_Recording("/camp/lab/schaefera/working/warnert/Recordings/jULIE recordings - 2019/Deep cortex recording/191029/2019-10-29_15-20-16", channel_count=16)
+
+bp_data = tc.set_threshold_crossings(return_bp=True, bp_indiv_chans=True, preprocess=True)
+tc.set_all_tcs_amplitudes(bp_data=bp_data)
---
+++ jULIE_recordings/jULIE_recording_setting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --job-name=setting_jULIE
+#SBATCH --output=/home/camp/warnert/bash_scripts/jULIE_recordings/setting.out
+#SBATCH --error=/home/camp/warnert/bash_scripts/jULIE_recordings/setting.err
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=250G
+#SBATCH --partition=cpu
+
+python window_classifier.py $SLURM_ARRAY_TASK_ID
