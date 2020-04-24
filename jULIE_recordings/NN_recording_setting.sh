5197ac5199621653b4237b9556ae45e621dfb912
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-24 12:35:19 +0100

    Create NN recording setters

---
+++ jULIE_recordings/NN_recording_setting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=setting_NN
+#SBATCH --array=0-5
+#SBATCH --output=/home/camp/warnert/bash_scripts/jULIE_recordings/setting_%a.out
+#SBATCH --error=/home/camp/warnert/bash_scripts/jULIE_recordings/setting_%a.err
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=250G
+#SBATCH --partition=cpu
+
+python NN_recording_setting.py $SLURM_ARRAY_TASK_ID
