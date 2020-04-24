c605df82004a4d6682d87703d651ba1ead9da42b
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-24 13:46:59 +0100

    Switch back to cpu after change to preprocessing

--- jULIE_recordings/NN_recording_setting.sh
+++ jULIE_recordings/NN_recording_setting.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=/home/camp/warnert/bash_scripts/jULIE_recordings/setting_%a.err
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem=500G
-#SBATCH --partition=hmem
+#SBATCH --mem=250G
+#SBATCH --partition=cpu
 
 python NN_recording_setting.py $SLURM_ARRAY_TASK_ID
