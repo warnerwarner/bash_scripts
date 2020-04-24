e804744478cdbe5e81d9013e5b85d77bee46371e
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-24 14:12:02 +0100

    Switch two of the recordings back to hmem

--- jULIE_recordings/NN_recording_setting.sh
+++ jULIE_recordings/NN_recording_setting.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 #SBATCH --job-name=setting_NN
-#SBATCH --array=0-5
+#SBATCH --array=3,4
 #SBATCH --output=/home/camp/warnert/bash_scripts/jULIE_recordings/setting_%a.out
 #SBATCH --error=/home/camp/warnert/bash_scripts/jULIE_recordings/setting_%a.err
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem=250G
-#SBATCH --partition=cpu
+#SBATCH --mem=500G
+#SBATCH --partition=hmem
 
 python NN_recording_setting.py $SLURM_ARRAY_TASK_ID
