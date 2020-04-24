f016f5be0a1c077e2cb53c6d6331676c489efbfb
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-24 17:18:58 +0100

    unique spike finder script

---
+++ jULIE_recordings/unique_spike_finder.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('/home/camp/warnert/neurolytics')
+import threshold_recording as tr
+import matplotlib.pyplot as plt
+import numpy as np
+from copy import deepcopy
+import os
+from tqdm import tqdm
+
+dir_index = int(list(sys.argv)[1])
+dirs = ["/home/camp/warnert/working/Recordings/2018_general/181205/2018-12-05_18-17-18",
+        '/home/camp/warnert/working/Recordings/2019_general/190121/2019-01-21_18-10-30',
+        "/home/camp/warnert/working/Recordings/2019_general/190207/2019-02-07_18-31-33",
+        "/home/camp/warnert/working/Recordings/2019_general/190211/2019-02-11_16-35-46",
+        "/home/camp/warnert/working/Recordings/2019_general/190704/2019-07-04_15-21-04",
+        "/home/camp/warnert/working/Recordings/2019_general/190801/2019-08-01_16-38-19"]
+chan_counts = [18, 18, 18, 18, 18, 32]
+
+tc = tr.Threshold_Recording(dirs[dir_index], channel_count=chan_counts[dir_index], dat_name='dat_for_jULIE_analysis.dat')
+
+tc.set()
+
+tc.find_unique_spikes()
---
+++ jULIE_recordings/unique_spike_finder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=setting_jULIE
+#SBATCH --array=0-5
+#SBATCH --output=/home/camp/warnert/bash_scripts/jULIE_recordings/setting_%a.out
+#SBATCH --error=/home/camp/warnert/bash_scripts/jULIE_recordings/setting_%a.err
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=200G
+#SBATCH --partition=cpu
+
+python jULIE_recording_setting.py $SLURM_ARRAY_TASK_ID
