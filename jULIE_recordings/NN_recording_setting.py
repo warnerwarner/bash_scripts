98a76d206d8c4169d2ce3cc1dab09d8caaa3ee2d
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-27 20:11:04 +0100

    change to single NN recording

--- jULIE_recordings/NN_recording_setting.py
+++ jULIE_recordings/NN_recording_setting.py
@@ -7,14 +7,14 @@ import numpy as np
 
 
 recording_index = int(list(sys.argv)[1])
+dirs = ['/home/camp/warnert/working/Recordings/Correlation_project_2019/190910/2019-09-10_12-42-15',
+        '/home/camp/warnert/working/Recordings/Correlation_project_2019/190911/2019-09-11_15-27-40',
+        '/home/camp/warnert/working/Recordings/Correlation_project_2019/190912/2019-09-12_14-55-50',
+        '/home/camp/warnert/working/Recordings/Correlation_project_2019/191008/2019-10-08_13-56-53',
+        '/home/camp/warnert/working/Recordings/Correlation_project_2019/191009/2019-10-09_14-48-27',
+        '/home/camp/warnert/working/Recordings/Correlation_project_2019/191010/2019-10-10_16-00-06']
 
-rec1 = tr.Threshold_Recording('/home/camp/warnert/working/Recordings/Correlation_project_2019/190910/2019-09-10_12-42-15', 32)
-rec2 = tr.Threshold_Recording('/home/camp/warnert/working/Recordings/Correlation_project_2019/190911/2019-09-11_15-27-40/', 32)
-rec3 = tr.Threshold_Recording('/home/camp/warnert/working/Recordings/Correlation_project_2019/190912/2019-09-12_14-55-50/', 32)
-rec4 = tr.Threshold_Recording('/home/camp/warnert/working/Recordings/Correlation_project_2019/191008/2019-10-08_13-56-53', 32)
-rec5 = tr.Threshold_Recording('/home/camp/warnert/working/Recordings/Correlation_project_2019/191009/2019-10-09_14-48-27', 32)
-rec6 = tr.Threshold_Recording('/home/camp/warnert/working/Recordings/Correlation_project_2019/191010/2019-10-10_16-00-06', 32)
-
-rec = [rec1, rec2, rec3, rec4, rec5, rec6][recording_index]
+rec_dir = dirs[recording_index]
+rec = tr.Threshold_Recording(rec_dir, 32)
 bp_data = rec.set_threshold_crossings(return_bp=True, bp_indiv_chans=True)
 rec.set_all_tcs_amplitudes(bp_data=bp_data)
--- jULIE_recordings/NN_recording_setting.sh
+++ jULIE_recordings/NN_recording_setting.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 #SBATCH --job-name=setting_NN
-#SBATCH --array=3,4
+#SBATCH --array=1,2,5
 #SBATCH --output=/home/camp/warnert/bash_scripts/jULIE_recordings/NNsetting_%a.out
 #SBATCH --error=/home/camp/warnert/bash_scripts/jULIE_recordings/NNsetting_%a.err
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem=500G
-#SBATCH --partition=hmem
+#SBATCH --mem=250G
+#SBATCH --partition=cpu
 
 python NN_recording_setting.py $SLURM_ARRAY_TASK_ID
