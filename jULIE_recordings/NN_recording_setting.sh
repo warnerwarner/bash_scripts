7448aeffa30cc533eee461fbd273bba45b9e442f
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-28 14:40:09 +0100

    Increase memory allocation for NN setting

--- jULIE_recordings/NN_recording_setting.sh
+++ jULIE_recordings/NN_recording_setting.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=/home/camp/warnert/bash_scripts/jULIE_recordings/NNsetting_%a.err
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
-#SBATCH --mem=250G
+#SBATCH --mem=350G
 #SBATCH --partition=cpu
 
 python NN_recording_setting.py $SLURM_ARRAY_TASK_ID
