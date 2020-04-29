7a1b64dd190a24613468c6ae452cff8d87385f66
warnerwarner <tom.p.a.warner@gmail.com>
2020-04-29 12:05:12 +0100

    Segmented up unique spike finding

---
+++ jULIE_recordings/NNunique_spike_finder190910.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=unique_spikes
+#SBATCH --array=0-31
+#SBATCH --output=/home/camp/warnert/bash_scripts/jULIE_recordings/NNunique_spikes_190910%a.out
+#SBATCH --error=/home/camp/warnert/bash_scripts/jULIE_recordings/NNunique_spikes_190910%a.err
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=100G
+#SBATCH --partition=cpu
+
+python NNunique_spike_finder190910.py $SLURM_ARRAY_TASK_ID
---
+++ jULIE_recordings/NNunique_spike_finder191009.py
@@ -0,0 +1,28 @@
+# Unique spike finding for '/home/camp/warnert/working/Recordings/Correlation_project_2019/191009/2019-10-09_14-48-27'
+
+import sys
+sys.path.append('/home/camp/warnert/neurolytics')
+import threshold_recording as tr
+import matplotlib.pyplot as plt
+import numpy as np
+from copy import deepcopy
+import os
+from tqdm import tqdm
+
+
+
+
+spike_slice = int(list(sys.argv)[1])
+total_slices = int(list(sys.argv)[2])
+
+start_time = spike_slice
+
+tc = tr.Threshold_Recording('/home/camp/warnert/working/Recordings/Correlation_project_2019/191009/2019-10-09_14-48-27', channel_count=32)
+
+tc.set()
+
+slice_size = tc.rec_length/total_slices
+start_time = spike_slice*slice_size
+end_time = start_time+slice_size
+
+tc.find_unique_spikes(start_time=start_time, end_time=end_time)
