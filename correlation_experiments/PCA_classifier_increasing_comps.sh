55259056b974405cde69441553b155b62599d8f8
warnerwarner <tom.p.a.warner@gmail.com>
2020-05-11 19:34:21 +0100

    Rename error and out file names

--- correlation_experiments/PCA_classifier_increasing_comps.sh
+++ correlation_experiments/PCA_classifier_increasing_comps.sh
@@ -2,8 +2,8 @@
 #
 #SBATCH --job-name=PCA_classifing
 #SBATCH --array=0-11	## N_TASKS_TOTAL: SET THIS TO THE NUMBER OF INDEPENDENT JOBS,TYPICLLAY 100,SEE BELOW N_TASKS_TOTAL
-#SBATCH --output=/home/camp/warnert/bash_scripts/correlation_experiments/increasing_PCA_comps_%a.out
-#SBATCH --error=/home/camp/warnert/bash_scripts/correlation_experiments/increasing_PCA_comps_%a.err
+#SBATCH --output=/home/camp/warnert/bash_scripts/correlation_experiments/increasing_PCA_comps_basic_%a.out
+#SBATCH --error=/home/camp/warnert/bash_scripts/correlation_experiments/increasing_PCA_comps_basic_%a.err
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=100G
