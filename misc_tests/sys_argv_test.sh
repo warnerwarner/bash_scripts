6f9c10a7231568a7ac5090e2c843564079e2a891
warnerwarner <tom.p.a.warner@gmail.com>
2020-05-12 18:03:21 +0100

    Update err and out locaitons for sys_arg_test

--- misc_tests/sys_argv_test.sh
+++ misc_tests/sys_argv_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=PCA_classifing
-#SBATCH --output=/home/camp/warnert/bash_scripts/misc_test/sys_arv.out
-#SBATCH --error=/home/camp/warnert/bash_scripts/misc_test/sys_arv.err
+#SBATCH --output=/home/camp/warnert/bash_scripts/misc_tests/sys_arv.out
+#SBATCH --error=/home/camp/warnert/bash_scripts/misc_tests/sys_arv.err
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=1G
