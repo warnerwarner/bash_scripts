4ccfc8f834e340903627f51d36656896c6469e9c
warnerwarner <tom.p.a.warner@gmail.com>
2020-05-12 19:04:32 +0100

    change error file

--- correlation_experiments/PCA_classifier_ordered_single_comp.sh
+++ correlation_experiments/PCA_classifier_ordered_single_comp.sh
@@ -2,8 +2,8 @@
 #
 #SBATCH --job-name=PCA_classifing
 #SBATCH --array=0-598    ## N_TASKS_TOTAL: SET THIS TO THE NUMBER OF INDEPENDENT JOBS,TYPICLLAY 100,SEE BELOW N_TASKS_TOTAL
-#SBATCH --output=/home/camp/warnert/bash_scripts/correlation_experiments/increasing_PCA_comps.out
-#SBATCH --error=/home/camp/warnert/bash_scripts/correlation_experiments/increasing_PCA_comps.err
+#SBATCH --output=/home/camp/warnert/bash_scripts/correlation_experiments/increasing_PCA_comps_single_%a.out
+#SBATCH --error=/home/camp/warnert/bash_scripts/correlation_experiments/increasing_PCA_comps_single_%a.err
 #SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --mem=150G
