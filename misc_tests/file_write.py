529c4a4804ac2427d496a5bf72e56e77ed7bd336
warnerwarner <tom.p.a.warner@gmail.com>
2020-05-12 15:15:08 +0100

    Cast int to string

--- misc_tests/file_write.py
+++ misc_tests/file_write.py
@@ -4,9 +4,9 @@ import os
 output_dir = 'test_output.txt'
 for i in range(100):
     f = open(output_dir, 'a')
-    f.write(i)
+    f.write(str(i))
     f.write(' ')
-    f.write(time.time())
+    f.write(str(time.time()))
     f.write('\n')
     f.close()
     time.sleep(10)
